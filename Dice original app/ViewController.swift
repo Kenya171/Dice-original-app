04de05f625a0c1f6e5cfa5147a779117ad9ef2f8
Kenya171 <fujikawa.k569@gmail.com>
2023-02-08 23:12:20 +0900

    Initial Commit

---
+++ Dice original app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice original app
+//
+//  Created by 藤川賢也 on 2023/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
